0e0db6e77bf4494504c31c161bf72e6599978d02
Yash Mishra <myash7166@gmail.com>
2026-09-01 23:30:30 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['images.unsplash.com', 'raw.githubusercontent.com', 'github.com'],
-  },
-};
-
-module.exports = nextConfig;
